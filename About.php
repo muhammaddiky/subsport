5fff34c5389aa6da6192305d914b6074b88e17b4
anitanusari <43595745+anitanusari@users.noreply.github.com>
2018-11-14 14:33:38 +0700

    Anita

---
+++ About.php
@@ -0,0 +1,117 @@
+<?php include  'header.php'; ?>
+<html>
+<body>
+
+  <nav class="navbar navbar-right">
+    <div class="container-fluid">
+      <li style="list-style-type:none;"><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+      &nbsp;
+      &nbsp;
+      &nbsp;
+      <li style="list-style-type:none;"><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      &nbsp;
+      &nbsp;
+    </div>
+  </nav>
+
+  <div class="header">
+    <img src="https://i.postimg.cc/9XvLppn4/Subsport3.png" style="width:240px;height:135px;" class="center">
+  </div>
+    <div class="topnav">
+  <a href="index.php">HOME</a>
+  <a class="active" href="About.php">ABOUT</a>
+  <a href="contactus.php">CONTACT US</a>
+  <a href="help.php">HELP</a>
+</div>
+  <div id="Home" class="tabcontent">
+    <a href="subsport - Copy.html"></a>
+  </div>
+
+  <div style="padding-left:16px">
+  </div>
+  <div id="inner" class="">
+    <div class="container">
+      <div id="content-wrapper" class="row "></div>
+      <div class="vc_row wpb_row vc_row-fluid vc_custom_1436943796254"></div>
+      <div class="vc_col-sm-12 wpb_column vc_column_container "></div>
+      <div class="wpb_wrapper"></div>
+      <tr>
+        <th>
+          <br>
+          <font size="6"> PROFILE
+        </th>
+        </font> <br> </center>
+      </tr>
+      <p>
+        <span align="justify">
+          <font size="3"> Kami adalah toko online yang menjual aneka sepatu online pilihan berbagai merk. Aneka varian sepatu sport ada ditoko kami, jadi anda tidak perlu datang ke toko sepatu yang berada di mall-mall yang mungkin lokasinya jauh
+            dari tempat tinggal anda. cukup memesan ditempat kami dan pilih sepatu favorit anda, kami pastikan pesanan anda akan langsung kamu kirim dan sampai ketempat anda. Semoga keberadaan kami menjadi salah satu tempat belanja favorit anda
+            khususnya untuk produk-produk sepatu sport. jika anda memerlukan informasi lebih detail tentang produk-produk kami ataupun hanya sekedar berkonsultasi silahkan hubungi kami. Dengan bangga dan senang hati kami akan melayani anda. Selamat
+            berbelanja ditempat kami, Terima kasih... </font>
+        </span><br>
+        <p><br>
+          <br>
+          <tr>
+            <th>
+              <font size="6"> OUR TEAM
+            </th>
+            </font> <br> </center>
+          </tr>
+          <img src="https://i.postimg.cc/k4FbrXB6/diky.jpg" style="width: 10%; margin-left: 22%; border-radius: 50%; margin-top: 3%">
+          <img src="https://i.postimg.cc/L4VZsXvN/Ilham.jpg" style="width: 11%; margin-left: 12%; border-radius: 50%; margin-top: 3%">
+          <img src="https://i.postimg.cc/JhLFKsL9/puput.jpg" style="width: 10%; margin-left: 12%;border-radius: 50%;  margin-top: 3%">
+          <img src="https://i.postimg.cc/tJNJZZ9p/galanta.jpg" style="width: 10%; margin-left: 33%; border-radius: 50%; margin-top: 5%">
+          <img src="https://i.postimg.cc/bNFGPP5V/anita.jpg" style="width: 10%; margin-left: 15%; border-radius: 75%;  margin-top: 5%">
+          <div class="clear"></div>
+          <tr>
+            <th>
+              <font size="6"> VISION
+            </th>
+            </font> <br> </center>
+          </tr>
+          <div>
+            <p>
+              <span align="justify">
+                <font size="3"> Untuk WEB Aplikasi dibentuk guna untuk menyelesaikan tugas Final Project dan sebagai pembelajaran bagi kelompok kami maupun kelompok lain yang ingin belajar mengenai Pemrograman Berbasis Web, serta ilmu yang semakin
+                  meluas menuju perdamaian dan kemakmuran untuk Indonesia sebagai penerus Bangsa !!!</font>
+              </span><br>
+              <span align="justify">
+                <font size="3"> Pada Web Aplikasi dari kelompok kami yaitu Subsport Station yaitu menjual sepatu sport dengan memudahkan para pembeli dengan mengunjungi Web Apikasi kami</font>
+              </span><br>
+              <p><br>
+                <br>
+          </div>
+          <tr>
+            <th>
+              <font size="6"> MISSION
+            </th>
+            </font> <br> </center>
+          </tr>
+          <div>
+            <p>
+              <span align="justify">
+                <font size="3"> 1. Menciptakan budaya kerja kelompok </font>
+              </span><br>
+              <span align="justify">
+                <font size="3"> 2. Merangkul cinta damai </font>
+              </span><br>
+              <span align="justify">
+                <font size="3"> 3. Mendorong kemajuan Teknologi </font>
+              </span><br>
+              <span align="justify">
+                <font size="3"> 4. Perkembangan dan Pembelajaran terus menerus </font>
+              </span><br>
+              <br>
+          </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+  <div class="footer">
+    <h3>Subsport Station</h3>
+    <p>Universitas Pembangunan Nasional "Veteran" Jawa Timur</p>
+  </div>
+
+
+</body>
+
+</html>
